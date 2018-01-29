065030f5b04d90b59e3f93e1db191593ad482a08
liyn <15901352542@163.com>
2018-01-29 10:09:27 +0800

    add start1

--- src/main/java/middle/main/Main.java
+++ src/main/java/middle/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String [] args){
         System.out.println("start...");
         System.out.println("end...");
+        System.out.println("start 1...");
     }
 }
