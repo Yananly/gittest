20c1d7f238986f71d2ac4ce3f1584b6b74c57709
liyn <15901352542@163.com>
2018-01-29 10:32:06 +0800

    changes

--- src/main/java/middle/main/Main.java
+++ src/main/java/middle/main/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String [] args){
         System.out.println("start...");
         System.out.println("end...");
+        System.out.println("start 1...");
+        System.out.println("end 1...");
     }
 }
